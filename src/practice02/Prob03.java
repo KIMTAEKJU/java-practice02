8d060a99bfc5ab02dbbce8e2ff3b39b55cd7213c
bit <bit@DESKTOP-0G705V6>
2018-12-27 12:47:20 +0900

    문제 해결했습니다 (틀렸다면 수정하겠습니다)

--- src/practice02/Prob03.java
+++ src/practice02/Prob03.java
@@ -1,10 +1,21 @@
 package practice02;
 
-public class Prob03 {
+import java.util.Scanner;
 
-	public static void main(String args[]) {
-		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+public class Prob03 
+{
 
+	public static void main(String args[]) 
+	{
+		Scanner scan = new Scanner(System.in);
+		//Prob03 p03 = new Prob03();
+		
+		String str = scan.nextLine(); // 문자열 입력
+		char c[] = new char[str.length()]; // 문자열 크기만큼 배열할당
+		
+		for (int i = 0; i < str.length(); i++)
+			c[i] = str.charAt(i);
+		
 		// 원래 배열 원소 출력
 		printCharArray(c);
 
@@ -15,4 +26,17 @@ public class Prob03 {
 		printCharArray(c);
 	}
 	
+	public static void printCharArray(char[] c)
+	{
+		for (int i = 0; i < c.length; i++)
+			System.out.print(c[i]);
+		System.out.println();
+	}
+	
+	public static void replaceSpace(char[] c)
+	{
+		for (int i = 0; i < c.length; i++)
+			if (c[i] == ' ')
+				c[i] = ',';
+	}
 }
